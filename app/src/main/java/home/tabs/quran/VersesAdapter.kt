10ae3003495b4cb375e015dccebebfb70b661c6d
NadineMahmoud <162837441+NadineMahmoud@users.noreply.github.com>
2025-04-17 13:28:04 +0200

    1

--- app/src/main/java/home/tabs/quran/VersesAdapter.kt
+++ app/src/main/java/home/tabs/quran/VersesAdapter.kt
@@ -1,4 +1,27 @@
 package home.tabs.quran
 
-class VersesAdapter {
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import androidx.recyclerview.widget.RecyclerView.ViewHolder
+import com.example.islamy.databinding.ItemVerseBinding
+
+class VersesAdapter(val verses : List<String>) :RecyclerView.Adapter<ViewHolder>(){
+
+    class viewHolder (val binding: ItemVerseBinding): RecyclerView.ViewHolder(
+
+        binding.root
+    )
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ViewHolder {
+        TODO("Not yet implemented")
+    }
+
+    override fun getItemCount(): Int = verses.size
+
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        TODO("Not yet implemented")
+    }
+
+
 }
